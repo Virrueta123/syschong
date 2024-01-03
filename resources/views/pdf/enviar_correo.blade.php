bcdfaa0b7fa0e94524dbc479fc60625ac46b7498
juan alex <juan_AVC789@hotmail.com>
2024-01-03 11:42:29 -0500

    dsadsa

--- resources/views/pdf/enviar_correo.blade.php
+++ resources/views/pdf/enviar_correo.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Ejemplo de Card con Bootstrap</title>
+  <link rel="icon" href="{{ asset('images/empresa/logo.png') }}" type="image/png">
   <!-- Enlace al archivo CSS de Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
@@ -11,7 +12,7 @@
 
 <div class="container mt-4">
   <div class="card" style="width: 18rem;">
-    <img src="{{asset('/')}}images/empresa/logo-chong.png" width="80" class="card-img-top" alt="Imagen">
+    <img src="{{asset('/')}}images/empresa/logo.pmg" width="80" class="card-img-top" alt="Imagen">
     <div class="card-body">
       <h5 class="card-title">{{$mensaje}}</h5> 
       <a href="{{$ruta}}" class="btn btn-primary">Mirar Comprobante</a>
